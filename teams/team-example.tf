cf08dbf81ddfc59f411057bb1a268ffcf4b0bde0
Ben Ngo <bengo@estee.com>
2023-08-17 14:01:17 -0500

    testing import

--- teams/team-example.tf
+++ teams/team-example.tf
@@ -1,9 +1,9 @@
-module "tf-repo" {
+module "repository" {
   source  = "mineiros-io/repository/github"
   version = "~> 0.18.0"
 
-  name               = "tf-github-demo"
-  license_template   = "gpl-3.0"
+  name               = "demo-repo"
+  license_template   = "apache-2.0"
   gitignore_template = "Terraform"
   visibility         = "private"
 }
