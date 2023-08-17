64eeb53871bfca0bcf1aa58c7ca8b2309c6688db
Ben Ngo <bengo@estee.com>
2023-08-17 13:29:18 -0500

    adding repo

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # tf-github-demo
+
+Goal of this demo is to leverage open-source moduels to manage github repo settings
+
+Uses the module https://github.com/mineiros-io/terraform-github-repository to manage settings
--- foundations.tf
+++ foundations.tf
@@ -7,3 +7,55 @@ module "repository" {
   gitignore_template = "Terraform"
   visibility         = "private"
 }
+
+
+
+module "perl-demo" {
+  source  = "mineiros-io/repository/github"
+  version = "~> 0.18.0"
+
+  name                   = "perl-demo"
+  license_template       = "apache-2.0"
+  visibility             = "private"
+  description            = "perl-demo"
+  homepage_url           = "https://github.com/ben-demo-org/perl-demo"
+  topics                 = ["random","stuff" ]
+  has_issues             = true
+  has_projects           = true
+  has_wiki               = true
+  has_downloads          = true
+  default_branch         = "main"
+  allow_squash_merge     = false
+  allow_merge_commit     = true
+  allow_rebase_merge     = false
+  delete_branch_on_merge = true
+  vulnerability_alerts   = true
+
+  admin_team_ids = [
+    "perl"
+  ]
+
+  branch_protection_v4 = [
+    {
+      branch                 = "main"
+      enforce_admins         = true
+
+      required_status_checks = {
+        strict   = true
+        contexts = []
+      }
+
+      required_pull_request_reviews = {
+        dismiss_stale_reviews           = true
+        dismissal_teams                 = []
+        require_code_owner_reviews      = true
+        required_approving_review_count = 0
+      }
+
+      restrictions = {
+        teams = ["perl"]
+      }
+    }
+  ]
+
+}
